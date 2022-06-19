b1815f6648befe56a86930aa9c5b5c9032b87b9f
Matt <matt.garvin.416@gmail.com>
2022-06-18 21:01:38 -0400

    added storage

--- lib/main.dart
+++ lib/main.dart
@@ -13,13 +13,53 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-   
+  String _haveStarted3Times = ''; 
+
+  @override
+  void initState() {
+    super.initState();
+    _incrementStartup();
+  }
+
+  Future<int> _getIntFromSharedPref() async {
+    final prefs = await SharedPreferences.getInstance();
+    final startupNumber = prefs.getInt('startupNumber');
+    if ( startupNumber == null ) {
+      return 0;
+    }
+    return startupNumber;
+  }
+
+  Future<void> _resetCounter()  async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt('startupNumber', 0);
+  }
+
+  Future<void> _incrementStartup()  async {
+    final prefs = await SharedPreferences.getInstance();
+    int lastStartupNumber = await _getIntFromSharedPref();
+    int currentStartupNumber = ++lastStartupNumber;
+
+    await prefs.setInt('startupNumber', currentStartupNumber);
+
+
+      setState(() {
+        _haveStarted3Times = 'app loaded ' + currentStartupNumber.toString() + ' times';
+      });
+    
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('body'),
+          child: Text(
+            _haveStarted3Times,
+            style: TextStyle(
+              fontSize: 32
+            ),
+          ),
         ),
       ),
     );
